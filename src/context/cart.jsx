f07e89a70ae9fcd2560e85bbf4d027cfe28df821
Mike <migueltenorio10617@gmail.com>
2023-06-11 16:05:13 -0600

    add use reducer

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -1,42 +1,33 @@
-import { createContext, useState } from "react";
-
+import { createContext, useReducer, useState } from "react";
+import { cartReducer, initialState } from "../reducer/cartReducer";
 export const CartContext = createContext();
 
-export function CartProvider({ children }){
-  const [cart, setCart] = useState([]);
-  const addToCart = product => {
-    // check if the product is already in the cart
-    const productInCart = cart.findIndex(item => item.id === product.id);
-    console.log(productInCart)
-    if(productInCart >= 0){
-      const newCart = structuredClone(cart);
-      newCart[productInCart].quantity += 1;
-      setCart(newCart);
-      return;
-    }
-    //producto no esta en el carrito
-    setCart(prevState => ([
-      ...prevState,
-      {
-        ...product,
-        quantity:1
-      }
-    ]))
-  }
-  const clearCart = () => {
-    setCart([])
-  }
+function useCartReducer(){
+  const [state, dispatch] = useReducer(cartReducer, initialState);
 
-  const removeFromCart = product => {
-    setCart(prevState => prevState.filter(item => item.id !== product.id))
-  }
+  const addToCart = product => dispatch({
+    type: "ADD_TO_CART",
+    payload: product,
+  })
+  const removeFromCart = product => dispatch({
+    type: "REMOVE_FROM_CART",
+    payload: product,
+  })
+  const clearCart = () => dispatch({
+    type: "CLEAR_CART"
+  })
+  return { state, addToCart, removeFromCart, clearCart}
+}
 
-  return(
-  <CartContext.Provider
-  value={{ 
-    cart, addToCart,clearCart, removeFromCart
-  }}>
-    {children}
-  </CartContext.Provider>
-  ) 
+export function CartProvider({ children }) {
+  const {state, addToCart, removeFromCart, clearCart} = useCartReducer();
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart: state, addToCart, clearCart, removeFromCart
+      }}>
+      {children}
+    </CartContext.Provider>
+  )
 }
